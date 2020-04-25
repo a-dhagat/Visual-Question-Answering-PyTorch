b81d0971687b7fb75cb5e62a89c0d37c38176d5a
a-dhagat <adhagat@andrew.cmu.edu>
2020-04-25 18:03:18 -0400

    Update softmax layer and word embedding layer

--- master/student_code/simple_baseline_net.py
+++ master/student_code/simple_baseline_net.py
@@ -4,8 +4,9 @@ from external.googlenet.googlenet import googlenet
 # from .. import external
         
 def convert_to_bow(question_encoding):
-    bow_freq_encoding = torch.sum(question_encoding)
-    bow_binary_encoding = torch.where(bow_freq_encoding>0.0, torch.tensor(1.0), torch.tensor(0.0))
+    # import ipdb; ipdb.set_trace()
+    bow_freq_encoding = torch.sum(question_encoding, dim=1)
+    bow_binary_encoding = torch.where(bow_freq_encoding>0.0, torch.tensor(1.0).cuda(), torch.tensor(0.0).cuda())
     return bow_binary_encoding
 
 class SimpleBaselineNet(nn.Module):
@@ -13,29 +14,37 @@ class SimpleBaselineNet(nn.Module):
     Predicts an answer to a question about an image using the Simple Baseline for Visual Question Answering (Zhou et al, 2017) paper.
     """
     def __init__(self):
-        super(SimpleBaselineNet, self).__init__()
+        super().__init__()
 	    ############ 2.2 TODO
         # import ipdb; ipdb.set_trace()
         # in_dim = 1024 + 5717 // 1000 + (5717->1024)
         # hid_dim = 
         # out_dim = 
-        self.reduce_to_low_dim = nn.Linear(5717, 1024)
         self.googlenet = googlenet(pretrained=True)
-        # self.classifier = nn.Sequential(
-        #     nn.Linear(in_dim, hid_dim),
-        #     nn.ReLU(),
-        #     nn.Dropout(0.4,inplace=True),
-        #     nn.Linear(hid_dim, out_dim),
-        # )
+        # for param in self.googlenet.parameters():
+
+        # self.branch_img_feats = googlenet(pretrained=True)
+        self.word_embeddings = nn.Sequential(nn.Linear(5747, 1024))
+        in_dim = 1024 + 1000
+        hid_dim = 1024
+        out_dim = 5217
+        self.softmax_layer = nn.Sequential(
+            nn.Linear(in_dim, out_dim),
+            # nn.ReLU(),
+            # nn.Dropout(0.4,inplace=True),
+            # nn.Linear(hid_dim, out_dim),
+        )
 
 	    ############
 
     def forward(self, image, question_encoding):
 	    ############ 2.2 TODO
+        # import ipdb; ipdb.set_trace()
         img_feat = self.googlenet(image)
         bow_encoding = convert_to_bow(question_encoding)
-        question_low_dim = self.reduce_to_low_dim(bow_encoding)
-        # pred = self.classifier(torch.cat(img_feat, question_low_dim), dim=1)
+        question_low_dim = self.word_embeddings(bow_encoding)
+        concat_vec = torch.cat((img_feat.transpose(1,0), question_low_dim.transpose(1,0))).transpose(1,0)
+        pred = self.softmax_layer(concat_vec)
         return pred
 
 	    ############
