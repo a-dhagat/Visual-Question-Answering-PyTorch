a2420173e962b604286fef4071e748e50f536d7b
a-dhagat <adhagat@andrew.cmu.edu>
2020-04-25 17:50:25 -0400

    Updating googlenet

--- master/external/googlenet/googlenet.py
+++ master/external/googlenet/googlenet.py
@@ -134,13 +134,13 @@ class GoogLeNet(nn.Module):
         # N x 1024 x 1 x 1
         x = x.view(x.size(0), -1)
         # N x 1024
-        if self.img_feat:
-            return x
+        # if self.img_feat:
+        #     return x
         x = self.dropout(x)
         x = self.fc(x)
         # N x 1000 (num_classes)
-        if self.training and self.aux_logits:
-            return aux1, aux2, x
+        # if self.training and self.aux_logits:
+        #     return aux1, aux2, x
         return x
 
 
